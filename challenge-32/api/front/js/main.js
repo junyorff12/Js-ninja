b632ad881bf657aa26a6635f6d284c02abb57c2d
junyorff12 <junyorff12@gmail.com>
2019-05-10 11:17:56 -0300

    api ajax pt 2

--- challenge-32/api/front/js/main.js
+++ challenge-32/api/front/js/main.js
@@ -2,13 +2,13 @@
   'use strict';
 
   let ajax = new XMLHttpRequest();
-  ajax.open('GET', 'http://localhost:3000/user', true);
+  ajax.open('GET', 'http://localhost:3000/user/pedro', true);
   ajax.send();
 
   ajax.onreadystatechange = function() {
-    if(ajax.readyState === 4 && ajax.status === 200 ){
-      console.log('Ajax');
+    if(ajax.readyState === 4 ){
+      console.log(ajax.responseText, ajax.status);
     }
   }
 
-})();
+})(); 
--- challenge-32/api/rest-api/index.js
+++ challenge-32/api/rest-api/index.js
@@ -1,7 +1,25 @@
 'use strict';
 
 let express = require('express');
+let cors = require('cors');
 let app = express();
+
+let users = {
+   jose: {
+      nome: 'Jose',
+      idade: 22
+   },
+   maria: {
+      nome: 'Maria',
+      idade: 25
+   },
+   ff12: {
+      nome: 'FF12',
+      idade: 32
+   }
+}
+
+app.use(cors());
 
 app.get('/', function(req, res){
    res.send('<h1>Home</h1>');
@@ -10,5 +28,14 @@ app.get('/', function(req, res){
 app.get('/user', (req, res) => {
    res.send('User');
 })
+
+app.get('/user/:username', (req, res) => {
+   let user = req.params.username
+   if( users[user]){
+      return res.json(users[user]);
+   }
+   res.status(404).json({error: 'Usuario não encontrado!'});
+})
+
 
 app.listen(3000);
