1aefd8dc1ed13dfbf05ea5bd73ff2793eb4d1f26
junyorff12 <junyorff12@gmail.com>
2019-05-17 12:33:15 -0300

    car store 32 part 1

--- challenge-32/challenge-32-1.js
+++ challenge-32/challenge-32-1.js
@@ -1,109 +1,110 @@
-(function($) {
+/*
+Agora vamos criar a funcionalidade de "remover" um carro. Adicione uma nova
+coluna na tabela, com um botão de remover.
+
+Ao clicar nesse botão, a linha da tabela deve ser removida.
+
+Faça um pull request no seu repositório, na branch `challenge-31`, e cole
+o link do pull request no `console.log` abaixo.
+
+Faça um pull request, também com a branch `challenge-31`, mas no repositório
+do curso, para colar o link do pull request do seu repo.
+*/
+(function(DOM) {
   'use strict';
 
-  /*
-  Vamos estruturar um pequeno app utilizando módulos.
-  Nosso APP vai ser um cadastro de carros. Vamos fazê-lo por partes.
-  A primeira etapa vai ser o cadastro de veículos, de deverá funcionar da
-  seguinte forma:
-  - No início do arquivo, deverá ter as informações da sua empresa - nome e
-  telefone (já vamos ver como isso vai ser feito)
-  - Ao abrir a tela, ainda não teremos carros cadastrados. Então deverá ter
-  um formulário para cadastro do carro, com os seguintes campos:
-    - Imagem do carro (deverá aceitar uma URL)
-    - Marca / Modelo
-    - Ano
-    - Placa
-    - Cor
-    - e um botão "Cadastrar"
-
-  Logo abaixo do formulário, deverá ter uma tabela que irá mostrar todos os
-  carros cadastrados. Ao clicar no botão de cadastrar, o novo carro deverá
-  aparecer no final da tabela.
-
-  Agora você precisa dar um nome para o seu app. Imagine que ele seja uma
-  empresa que vende carros. Esse nosso app será só um catálogo, por enquanto.
-  Dê um nome para a empresa e um telefone fictício, preechendo essas informações
-  no arquivo company.json que já está criado.
-
-  Essas informações devem ser adicionadas no HTML via Ajax.
-
-  Parte técnica:
-  Separe o nosso módulo de DOM criado nas últimas aulas em
-  um arquivo DOM.js.
-
-  E aqui nesse arquivo, faça a lógica para cadastrar os carros, em um módulo
-  que será nomeado de "app".
-  */
-  let app = (function() {
+  let app = (function(){
     return {
-      init: function init() {
-        this.companyInfo();
+      init: function() {
+        this.getCompanyInfo();
         this.initEvents();
       },
 
-      initEvents: function initEvents() {
-        $('[data-js="form-carros"]').on('submit', this.handleSubmit, false);
-      },
-
-      handleSubmit: function handleSubmit(e) {
-        e.preventDefault();
-        console.log('submit');
-        let $tableCars = $('[data-js="table-car"]').get();
-        $tableCars.appendChild(app.createNewCar());
-      },
-
-      createNewCar: function createNewCar() {
-        let $fragment = document.createDocumentFragment();
-        let $tr = document.createElement('tr');
-        let $tdImage = document.createElement('td');
-        let $image = document.createElement('img');
-        let $tdBrand = document.createElement('td');
-        let $tdYear = document.createElement('td');
-        let $tdPlate = document.createElement('td');
-        let $tdColor = document.createElement('td');
-
-        $image.src = $('[data-js="input-img" ]').get().value;
-        $tdImage.appendChild($image);
-
-        $tdBrand.textContent = $('[data-js="input-marca" ]').get().value;
-        $tdYear.textContent = $('[data-js="input-ano" ]').get().value;
-        $tdPlate.textContent = $('[data-js="input-placa" ]').get().value;
-        $tdColor.textContent = $('[data-js="input-cor" ]').get().value;
-
-        $tr.appendChild($tdImage);
-        $tr.appendChild($tdBrand);
-        $tr.appendChild($tdYear);
-        $tr.appendChild($tdPlate);
-        $tr.appendChild($tdColor);
-
-        return $fragment.appendChild($tr);
-      },
-
-      companyInfo: function companyInfo() {
+      getCompanyInfo: function getCompanyInfo(){
         let ajax = new XMLHttpRequest();
         ajax.open('GET', '/company.json', true);
         ajax.send();
-        ajax.addEventListener('readystatechange', this.getCompanyInfo, false);
+        ajax.addEventListener('readystatechange', this.getData, false);
+        
       },
 
-      getCompanyInfo: function getCompanyInfo() {
-        if (!app.isReady.call(this)) {
+      getData: function getData() {
+        if(!(app.isRequestOK.call(this)))
           return;
-        }
+        let $companyName = new DOM('[data-js="company-name"]').get();
+        let $companyPhone = new DOM('[data-js="company-phone"]').get();
         let data = JSON.parse(this.responseText);
-        let $companyName = new $('[data-js="company-name"]').get();
-        let $companyPhone = new $('[data-js="company-fone"]').get();
-
         $companyName.textContent = data.name;
         $companyPhone.textContent = data.phone;
       },
 
-      isReady: function isReady() {
+      isRequestOK: function isRequestOK() {
         return this.readyState === 4 && this.status === 200;
       },
-    };
-  })();
+
+      initEvents: function initEvents() {
+        let $formCar = new DOM('[data-js="form-car"]')
+        $formCar.on('submit', this.handleSubmit, false);
+      },
+
+      handleSubmit: function handleSubmit(e) {
+        e.preventDefault();
+        let $tableCar = new DOM('[data-js="car-table"]').get();
+        $tableCar.appendChild(app.saveNewCar());
+      },
+
+      removeLine: function removeLine() {
+        console.log('removendo linha');
+        this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode);
+      },
+
+      saveNewCar: function saveNewCar() {
+    
+        let $img = new DOM('[data-js="carImg"]').get();
+        let $modelBrand = new DOM('[data-js="carModelBrand"]').get();
+        let $year = new DOM('[data-js="carYear"]').get();
+        let $plate = new DOM('[data-js="carPlate"]').get();
+        let $color = new DOM('[data-js="carColor"]').get();
+
+        let fragment = document.createDocumentFragment();
+        let tr = document.createElement('tr');
+        let img = document.createElement('img');
+        let tdImg = document.createElement('td');
+        let tdModelBrand = document.createElement('td');
+        let tdYear = document.createElement('td');
+        let tdPlate = document.createElement('td');
+        let tdColor = document.createElement('td');
+        let tdRemove = document.createElement('td') // td remove
+        let $removeButton = document.createElement('button');
+        
+        $removeButton.style.width = '100%';
+        $removeButton.textContent = 'Remover';
+        $removeButton.addEventListener('click', this.removeLine);
+
+        img.src = $img.value;
+        tdModelBrand.textContent = $modelBrand.value;
+        tdYear.textContent = $year.value;
+        tdPlate.textContent = $plate.value;
+        tdColor.textContent = $color.value;
+        tdImg.appendChild(img);
+        tdRemove.appendChild($removeButton);
+        
+        tr.appendChild(tdImg);
+        tr.appendChild(tdModelBrand);
+        tr.appendChild(tdYear);
+        tr.appendChild(tdPlate);
+        tr.appendChild(tdColor);
+        tr.appendChild(tdRemove); // td remove
+
+        return fragment.appendChild(tr);
+      }
+    
+    } //fecha o return
+
+  })(); //fecha o app
 
   app.init();
+
 })(window.DOM);
+
+//console.log('Link do pull request do seu projeto');
